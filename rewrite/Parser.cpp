2713d78b012e9833cea2bfcbc7076e702b43ee9a
uped <mikikocz@gmail.com>
2024-06-26 22:28:00 +0200

    Parser prototype improvements.

--- rewrite/Parser.cpp
+++ rewrite/Parser.cpp
@@ -3,62 +3,65 @@
 #include <optional>
 
 Token Parser::advance() {
-    previous = current;
-    current = lexer.next_token().value(); // todo handle errors!
-    return current;
+    current = next;
+    next = lexer.next_token().value(); // todo handle errors!
+    return next;
 }
 
-Expr Parser::integer(const Token& token) {
+Expr Parser::integer() const {
     // todo temp
-    return LiteralExpr {std::stoi(previous.get_lexeme(lexer.get_source()))};
+    return LiteralExpr {std::stoi(current.get_lexeme(lexer.get_source()))};
 }
 
-Expr Parser::prefix(const Token &token) {
-    switch (previous.type) {
-        case Token::Type::INTEGER:
-            return integer(previous);
-        case Token::Type::BANG:
-            return UnaryExpr {make_unique<Expr>(expression(0)), previous.type};
-        default:
-            throw Error(current, "Unexpected token.");
-    }
+Expr Parser::unary(Token::Type op) {
+    return UnaryExpr { std::make_unique<Expr>(expression()), op };
 }
 
-// std::optional<Expr> Parser::infix(const Expr& expr, const Token &token) {
-//     Expr right = expression();
-//     switch (token.type) {
-//         case Token::Type::PLUS:
-//         case Token::Type::STAR:
-//             return BinaryExpr {std::make_unique<Expr>(std::move(expr)), std::make_unique<Expr>(std::move(right)), token.type};
-//     }
-// }
-
-
-int Parser::get_precendece() {
+Expr Parser::prefix() {
     switch (current.type) {
-        case Token::Type::PLUS:
-            return 3;
-        case Token::Type::STAR:
-            return 4;
+        case Token::Type::INTEGER:
+            return integer();
+        case Token::Type::BANG:
+            return unary(current.type);
         default:
-            return 0;
+            throw Error(next, "Unexpected token.");
     }
 }
 
-Expr Parser::expression(int precedence) {
+Expr Parser::infix(Expr left) {
+    switch (current.type) {
+        case Token::Type::STAR:
+        case Token::Type::PLUS:
+            return binary(std::move(left));
+        default:
+            return left;
+    }
+}
+
+
+Expr Parser::binary(Expr left) {
+    Token::Type op = current.type;
+    return BinaryExpr {std::make_unique<Expr>(std::move(left)),
+        std::make_unique<Expr>(expression(get_precendece(op))), op};
+}
+
+Parser::Precedence Parser::get_precendece(Token::Type token) {
+    switch (token) {
+        case Token::Type::PLUS:
+            return Precedence::TERM;
+        case Token::Type::STAR:
+            return Precedence::FACTOR;
+        default:
+            return Precedence::NONE;
+    }
+}
+
+Expr Parser::expression(Precedence precedence) {
     advance();
-    auto left = prefix(current);
-    while (precedence < get_precendece()) {
+    auto left = prefix();
+    while (precedence < get_precendece(next.type)) {
         advance();
-        auto bound = previous;
-        Expr right = expression(previous.type == Token::Type::PLUS ? 3 : 4);
-        switch (bound.type) {
-            case Token::Type::PLUS:
-            case Token::Type::STAR:
-                return BinaryExpr {std::make_unique<Expr>(std::move(left)), std::make_unique<Expr>(std::move(right)), bound.type};
-            default:
-                return left;
-        }
+        left = infix(std::move(left));
     }
     return left;
 }
--- rewrite/Parser.h
+++ rewrite/Parser.h
@@ -11,29 +11,58 @@
  * https://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/
  * https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html
  * https://en.wikipedia.org/w/index.php?title=Operator-precedence_parser
+ * https://github.com/munificent/bantam/tree/master
  */
 class Parser {
 public:
+    enum class Precedence {
+        NONE,
+        ASSIGMENT,
+        LOGICAL_OR,
+        LOGICAL_AND,
+        BITWISE_OR,
+        BITWISE_AND,
+        BITWISE_XOR,
+        EQUALITY,
+        RELATIONAL,
+        BITWISE_SHIFT,
+        TERM,
+        FACTOR,
+        UNARY,
+        CALL,
+        PRIMARY // literal of variable
+    };
+
+
     class Error : public std::runtime_error {
     public:
         Error(Token token, std::string_view message) : std::runtime_error(message.data()), token(token) {}
         Token token;
     };
 
-    explicit Parser(const Lexer &lexer) : lexer(lexer) {advance();} // todo not final
-    Expr integer(const Token &token);
-    Expr prefix(const Token& token);
+    explicit Parser(const Lexer &lexer) : lexer(lexer) { // todo: not final
+        advance();
+    }
+    Expr integer() const;
 
-    std::optional<Expr> infix(const Expr& expr, const Token &token);
+    Expr unary(Token::Type op);
 
-    int get_precendece();
+    Expr prefix();
 
-    Expr expression(int precedence = 0);
+    Expr infix(Expr left);
+
+    Expr binary(Expr left);
+    Expr expression(Precedence precedence = Precedence::NONE);
 private:
+
+    static Precedence get_precendece(Token::Type token);
+
+
+
     Token advance();
 
-    Token previous;
     Token current;
+    Token next;
     Lexer lexer;
 };
 
