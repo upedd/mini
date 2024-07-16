f368133811c55729e289d779eec44402bfe93ed4
uped <mikikocz@gmail.com>
2024-07-16 23:34:17 +0200

    Add timeout to test runner

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -20,27 +20,35 @@ log = open(f"testrunner_{date_string}.log", "w")
 success_cnt = 0
 error_cnt = 0
 
+def fail_test(test_name, reason):
+    global error_cnt
+    error_cnt += 1
+    print("Test failed!")
+    log.write(f"Test {test_name} failed for reason: {reason}.\n")
+
 
 def traverse_directory(path):
-    global error_cnt
     global success_cnt
     for file_path in Path(path).rglob('*'):
         if file_path.is_file():
             if file_path.suffix == ".bite":
                 print(f"Running {file_path.stem} test...")
-                result = run_executable(BITE_PATH, f".\\{file_path}")
+                try:
+                    result = subprocess.run([BITE_PATH, f".\\{file_path}"], check=True, text=True, capture_output=True, timeout=4)
+                except subprocess.CalledProcessError as e:
+                    fail_test(file_path.stem, "execution failed")
+                    continue
+                except subprocess.TimeoutExpired as e:
+                    fail_test(file_path.stem, "execution timeout")
+                    continue
                 f = open(file_path.with_suffix(".out"), "r")
                 out = f.read()
                 if result.stderr:
-                    error_cnt += 1
-                    print("Test failed!")
-                    log.write(f"Test {file_path.stem} failed for reason: error output.\n")
+                    fail_test(file_path.stem, "runtime error")
                     log.write("error:\n")
                     log.write(result.stderr)
                 elif out.strip() != result.stdout.strip():
-                    error_cnt += 1
-                    print("Test failed!")
-                    log.write(f"Test {file_path.stem} failed for reason: invalid output.\n")
+                    fail_test(file_path.stem, "invalid output")
                     log.write("Expected:\n")
                     log.write(out)
                     log.write('\n')
