066c01475d840aea64196628d916b758a3f561e0
uped <mikikocz@gmail.com>
2024-07-30 01:42:14 +0200

    Continue work on print.h helpers

--- source/base/print.h
+++ source/base/print.h
@@ -2,12 +2,14 @@
 #define PRINT_H
 #include <cassert>
 #include <cstdint>
+#include <ranges>
 #include <string>
+#include <utility>
+#include <vector>
 
 // Common utilites related to std::print
 
 namespace bite {
-
     // inspired by: https://github.com/fmtlib/fmt/blob/master/include/fmt/color.h
 
     // https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
@@ -45,55 +47,174 @@ namespace bite {
     class terminal_style {
     public:
         constexpr terminal_style(emphasis em) : emphasis(static_cast<uint8_t>(em)) {}
+
         constexpr terminal_style(const bool is_foreground, const terminal_color color) {
             if (is_foreground) {
-                has_foreground_color = true;
+                is_foreground_color_set = true;
                 foreground_color = color;
             } else {
-                has_foreground_color = true;
+                is_background_color_set = true;
                 background_color = color;
             }
         }
 
         constexpr terminal_style& operator|=(const terminal_style& rhs) {
-            if (rhs.has_foreground_color) {
-                if (this->has_foreground_color) {
-                    assert(false); // TODO: better error handling
-                }
-                this->foreground_color = rhs.foreground_color;
-                this->has_foreground_color = true;
-            }
-
-            if (rhs.has_background_color) {
-                if (this->has_foreground_color) {
+            if (rhs.is_background_color_set) {
+                if (this->is_background_color_set) {
                     assert(false); // TODO: better error handling
                 }
                 this->background_color = rhs.background_color;
-                this->has_background_color = true;
+                this->is_background_color_set = true;
+            }
+
+            if (rhs.is_foreground_color_set) {
+                if (this->is_foreground_color_set) {
+                    assert(false); // TODO: better error handling
+                }
+                this->foreground_color = rhs.foreground_color;
+                this->is_foreground_color_set = true;
             }
 
             this->emphasis |= rhs.emphasis;
+
+            return *this;
         }
+
+        constexpr terminal_style operator|(const terminal_style rhs) {
+            return *this |= rhs;
+        }
+
+        [[nodiscard]] constexpr terminal_color get_background_color() const { return background_color; }
+        [[nodiscard]] constexpr terminal_color get_foreground_color() const { return foreground_color; }
+        [[nodiscard]] constexpr bool has_background_color() const { return is_background_color_set; }
+        [[nodiscard]] constexpr bool has_foreground_color() const { return is_foreground_color_set; }
+        [[nodiscard]] constexpr uint8_t get_emphasis() const { return emphasis; }
+
     private:
         terminal_color background_color {};
         terminal_color foreground_color {};
-        bool has_background_color = false;
-        bool has_foreground_color = false;
+        bool is_background_color_set = false;
+        bool is_foreground_color_set = false;
         uint8_t emphasis = 0;
     };
 
     constexpr terminal_style foreground(terminal_color color) {
-        return {true, color};
+        return { true, color };
     }
 
     constexpr terminal_style background(terminal_color color) {
-        return {false, color};
+        return { false, color };
     }
 
-    namespace print::detail {
-        constexpr std::string get_ansi_escape(const terminal_style& style) {
-            std::string output {""};
+    namespace detail {
+        constexpr std::uint8_t map_emphasis_to_escape(const emphasis& em) {
+            switch (em) {
+                case emphasis::bold: return 1;
+                case emphasis::faint: return 2;
+                case emphasis::italic: return 3;
+                case emphasis::underline: return 4;
+                case emphasis::blink: return 5;
+                case emphasis::reverse: return 7;
+                case emphasis::conceal: return 8;
+                case emphasis::strikethrough: return 9;
+            }
+            std::unreachable();
         }
+
+        constexpr std::string get_ansi_escape(const terminal_style& style) {
+            std::vector<std::uint8_t> parameters;
+            if (style.has_foreground_color()) {
+                parameters.push_back(static_cast<std::uint8_t>(style.get_foreground_color()));
+            }
+            if (style.has_background_color()) {
+                parameters.push_back(static_cast<std::uint8_t>(style.get_background_color()) + 10);
+            }
+            for (std::uint8_t i = 0; i < 8; ++i) {
+                if (style.get_emphasis() & 1 << i) {
+                    parameters.push_back(map_emphasis_to_escape(static_cast<emphasis>(1 << i)));
+                }
+            }
+
+            // styling: fix weird formatting
+            std::string parameters_string = parameters | std::views::transform(
+                [](const uint8_t num) { return std::to_string(num); }
+            ) | std::views::join_with(';') | std::ranges::to<std::string>();
+
+            return "\033[" + parameters_string + 'm';
+        }
+    }
+
+
+    // Helpers for printing directly into cout
+    // These should be in std but are missing? https://en.cppreference.com/w/cpp/io/print
+    // for completness sake
+    template <typename... Args>
+    void print(std::ostream& ostream, std::format_string<Args...> string, Args&&... args) {
+        std::print(ostream, string, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void print(std::format_string<Args...> string, Args&&... args) {
+        bite::print(std::cout, string, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void println(std::ostream& ostream, std::format_string<Args...> string, Args&&... args) {
+        bite::print(ostream, "{}\n", std::format(string, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void println(std::format_string<Args...> string, Args&&... args) {
+        bite::println(std::cout, "{}\n", string, std::forward<Args>(args)...);
+    }
+
+    // Repeated printing helper
+    template <typename... Args>
+    void print_repeat(const std::size_t times, std::ostream& ostream, std::format_string<Args...> string, Args&&... args) {
+        for (std::size_t i = 0; i < times; ++i) {
+            bite::print(ostream, string, std::forward<Args>(args)...);
+        }
+    }
+
+    template <typename... Args>
+    void print_repeat(const std::size_t times, std::format_string<Args...> string, Args&&... args) {
+        bite::print_repeat(times, std::cout, string, std::forward<Args>(args)...);
+    }
+
+    // ===== Styled printing helpers
+    template <typename... Args>
+    void print(const terminal_style& style, std::ostream& ostream, std::format_string<Args...> string, Args&&... args) {
+        bite::print(ostream, "{}{}\033[0m", detail::get_ansi_escape(style), std::format(string, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void print(const terminal_style& style, std::format_string<Args...> string, Args&&... args) {
+        bite::print(style, std::cout, string, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    void println(const terminal_style& style, std::ostream& ostream, std::format_string<Args...> string, Args&&... args) {
+        bite::print(style, ostream, "{}\n", std::format(string, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void println(const terminal_style& style, std::format_string<Args...> string, Args&&... args) {
+        bite::println(style, std::cout, string, std::forward<Args>(args)...);
+    }
+
+
+    // Repeated printing helper
+    template <typename... Args>
+    void print_repeat(const terminal_style& style, const std::size_t times, std::ostream& ostream, std::format_string<Args...> string, Args&&... args) {
+        // optim: print ansi escape before printing these chars
+        for (std::size_t i = 0; i < times; ++i) {
+            bite::print(style, ostream, string, std::forward<Args>(args)...);
+        }
+    }
+
+    template <typename... Args>
+    void print_repeat(const terminal_style& style, const std::size_t times, std::format_string<Args...> string, Args&&... args) {
+        bite::print_repeat(style, times, std::cout, string, std::forward<Args>(args)...);
     }
 }
 
--- source/parser/ErrorPrettyPrinter.h
+++ source/parser/ErrorPrettyPrinter.h
@@ -5,6 +5,7 @@
 #include <list>
 
 #include "CompilationMessage.h"
+#include "../base/print.h"
 
 class ErrorPrettyPrinter {
 public:
@@ -51,73 +52,54 @@ public:
         }
     }
 
-    // These should be in std but are missing? https://en.cppreference.com/w/cpp/io/print
-
-    template <typename... Args>
-    void print(std::format_string<Args...> string, Args&&... args) {
-        std::print(std::cout, string, std::forward<Args>(args)...);
-    }
-
-    template <typename... Args>
-    void println(std::format_string<Args...> string, Args&&... args) {
-        std::println(std::cout, string, std::forward<Args>(args)...);
-    }
-
-    template <typename... Args>
-    void print_repeat(std::size_t times, std::format_string<Args...> string, Args&&... args) {
-        for (std::size_t i = 0; i < times; ++i) {
-            std::print(std::cout, string, std::forward<Args>(args)...);
-        }
-    }
-
     void print_padding(std::size_t padding) {
-        print_repeat(padding, " ");
+        bite::print_repeat(padding, " ");
     }
 
     void print_location(std::string_view path, std::size_t line_number, std::size_t line_offset) {
         if (terminal_compatibility_mode) {
-            print("--> ");
+            bite::print("--> ");
         } else {
-            print("\033[90m┌─>\033[0m ");
+            bite::print("\033[90m┌─>\033[0m ");
         }
-        println("{}:{}:{}", path, line_number, line_offset + 1);
+        bite::println("{}:{}:{}", path, line_number, line_offset + 1);
     }
 
     void print_main_message(std::string_view message) {
         if (terminal_compatibility_mode) {
-            println("error: {}", message);
+            bite::println("error: {}", message);
         } else {
-            println("\033[1;31merror\033[0m\033[1m: {}\033[0m", message);
+            bite::println("\033[1;31merror\033[0m\033[1m: {}\033[0m", message);
         }
     }
 
     void print_vbar() {
         if (terminal_compatibility_mode) {
-            print("|");
+            bite::print("|");
         } else {
-            print("\033[90m│\033[0m");
+            bite::print("\033[90m│\033[0m");
         }
     }
 
     // truncating main line
     void print_main_line(std::size_t line_number, std::string_view line) {
         if (terminal_compatibility_mode) {
-            println("{} | {}", line_number, line);
+            bite::println("{} | {}", line_number, line);
         } else {
-            println("\033[1m{}\033[0m \033[90m│\033[0m {}", line_number, line);
+            bite::println("\033[1m{}\033[0m \033[90m│\033[0m {}", line_number, line);
         }
     }
 
     void print_inline_message(std::size_t token_length, std::string_view message) {
         if (!terminal_compatibility_mode) {
-            print("\033[31m");
+            bite::print("\033[31m");
         }
-        print_repeat(token_length, "^");
-        print(" {}", message);
+        bite::print_repeat(token_length, "^");
+        bite::print(" {}", message);
         if (!terminal_compatibility_mode) {
-            println("\033[0m");
+            bite::println("\033[0m");
         } else {
-            println("");
+            bite::println("");
         }
     };
 
@@ -131,7 +113,7 @@ public:
 
             print_padding(padding);
             print_vbar();
-            println("");
+            bite::println(" ");
 
             print_main_line(msg.line_number, msg.line);
 
